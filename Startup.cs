9aa9e48c8888b4d004a4f70ec975946213dc5a49
Thales Reis <thales.areis@gmail.com>
2019-11-04 19:50:47 -0300

    add swagger

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace api
 {
@@ -33,6 +34,17 @@ namespace api
             );            
             services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "2DOList - API",
+                    Description = "This is the 2DOList API website",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "2DO List Team", Email = "support@2dolist.com", Url = "www.super2dolist.com" }
+                });
+            });            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +62,11 @@ namespace api
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "2DOList - API V1");
+            });            
         }
     }
 }
